d13320ca034d43421561f6e47ad2c481a17676fc
tdeng <tdeng@xogrp.com>
2015-07-21 17:40:46 +0800

    itest

--- app/models/concerns/rails_admin_dsl/organization.rb
+++ app/models/concerns/rails_admin_dsl/organization.rb
@@ -4,30 +4,35 @@ module RailsAdminDSL::Organization
     rails_admin do
       list do
         scopes [:include_addresses]
-        def list_config(field_value, label_value, column_width_value, formatted_value_1, formatted_value_2)
+        def list_config(field_value, label_value, column_width_value)
           field field_value do
             label label_value if label_value.present?
             column_width column_width_value if column_width_value.present?
+          end
+        end
+        def list_config_bindings_2(field_value, formatted_value_1, formatted_value_2)
+          field field_value do
             if formatted_value_1.present? && formatted_value_2.present?
               formatted_value { "#{bindings[:object][formatted_value_1]} #{bindings[:object][formatted_value_2]}" }
             end
           end
         end
-        def list_config_bindings(field_value, label_value, column_width_value, formatted_value)
+        def list_config_bindings_1(field_value, formatted_value_1)
           field field_value do
-            label label_value if label_value.present?
-            column_width column_width_value if column_width_value.present?
-            formatted_value { bindings[:object].addresses[0][formatted_value] if bindings[:object].addresses.present? } if formatted_value.present?
+            formatted_value { bindings[:object].addresses[0][formatted_value_1] if bindings[:object].addresses.present? } if formatted_value_1.present?
           end
         end
-        list_config(:id, 'ID', 40, '', '')
-        list_config(:name, 'Business Name', 100, '', '')
-        list_config(:contact_name, 'Contact Name', 100, :first_name, :last_name)
-        list_config(:type, 'Type', 100, '', '')
-        list_config_bindings(:city, 'City', 100, :city)
-        list_config_bindings(:state, 'State', 100, :state)
-        list_config(:expiration_date, 'Expiration Date', 100, '', '')
-        list_config(:activated_date, 'Last Active', 100, '', '')
+        list_config(:id, 'ID', 40)
+        list_config(:name, 'Business Name', 100)
+        list_config(:contact_name, 'Contact Name', 100)
+        list_config_bindings_2(:contact_name, :first_name, :last_name)
+        list_config(:type, 'Type', 100)
+        list_config(:city, 'City', 100)
+        list_config(:state, 'State', 100)
+        list_config_bindings_1(:city, :city)
+        list_config_bindings_1(:state, :state)
+        list_config(:expiration_date, 'Expiration Date', 100)
+        list_config(:activated_date, 'Last Active', 100)
       end
       edit do
         def edit_config(field_value, label_value, hide_value, help_value)
